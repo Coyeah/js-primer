03149dd34218790cfc39627307aa35581ec69186
Coyeah <coyeah_chen@outlook.com>
2018-06-12 22:16:57 +0800

    update: sigleton v1.0

---
+++ oop-restaurant/singleton/sigleton.js
@@ -0,0 +1,105 @@
+'use strict'
+// // ===== 单例demo1 ===== //
+// let Sigleton = (function () {
+//   // 默认为空，用于判断是否已实例化
+//   let instance = null;
+//   function init () {
+//     return {
+//       publicMethod: function () {
+//         console.log("Hello World!");
+//       },
+//       publicProperty: "v1.0"
+//     };
+//   }
+
+//   return {
+//     getInstance: function () {
+//       if (!instance) {
+//         instance = init();
+//       }
+//       return instance;
+//     }
+//   }
+// })();
+
+// Sigleton.getInstance().publicMethod();
+
+
+
+// // ===== 单例demo2 ===== //
+// let Sigleton = (function () {
+//   let instance = null;
+//   function SupportClass (args) {
+//     var args = args || {};
+//     this.name = args.name || "Coyeah";
+//     this.id = args.id || 123456;
+//     //检验单例模式
+//     console.log('this is created!');
+//   };
+
+//   SupportClass.prototype = {
+//     constructor: SupportClass,
+//     displayInfo: function () {
+//       console.log("name: " + this.name + " | id: " + this.id);
+//     }
+//   };
+
+//   return {
+//     // 类的名字
+//     name: 'SupportClass',
+//     getInstance: function (args) {
+//       if (instance === null) {
+//         instance = new SupportClass(args);
+//       }
+//       return instance;
+//     }
+//   }
+// })();
+
+// // 调用两次方法，只会被实例一次
+// Sigleton.getInstance();
+// Sigleton.getInstance();
+
+
+
+// // ===== 单例demo3 ===== //
+// function Sigleton (args) {
+//   if (typeof Sigleton.instance === 'object') {
+//     return Sigleton.instance;
+//   }
+
+//   var args = args || {};
+//   this.name = args.name || "Coyeah";
+//   this.id = args.id || 123456;
+//   Sigleton.instance = this;
+// }
+
+// Sigleton.prototype = {
+//   constructor: Sigleton,
+//   displayInfo: function () {
+//     console.log("name: " + this.name + " | id: " + this.id);
+//   }
+// }
+
+
+
+// ===== 单例demo4 ===== //
+function Sigleton (args) {
+  var instance = null;
+  var args = args || {};
+  this.name = args.name || "Coyeah";
+  this.id = args.id || 123456;
+  // 将instance引用创建的实例this
+  instance = this;
+  // 重写构造函数
+  Sigleton = function () {
+    return instance;
+  }
+};
+
+Sigleton.prototype = {
+  constructor: Sigleton,
+  displayInfo: function () {
+    console.log("name: " + this.name + " | id: " + this.id);
+  }
+}
