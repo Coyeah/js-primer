8fb8c3960da2083c67574b7989f664505fd79b53
Coyeah <Coyeah_chen@outlook.com>
2019-02-03 00:13:45 +0800

    Updated: config of request

---
+++ systemModule/Request/config.js
@@ -0,0 +1,11 @@
+export const I64BIT_TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-'.split('');
+
+export const UPPER_NUMBER = Math.pow(2, 53);
+
+export const MAX_GUARANTEE_AMOUNT = 99999999;
+
+export default {
+  I64BIT_TABLE,
+  UPPER_NUMBER,
+  MAX_GUARANTEE_AMOUNT,
+};
